4707d5d98dc42df1af619e5486da9f756092f574
buketbyk <bukettbayik@gmail.com>
2022-05-21 20:49:10 +0300

    updated

--- lib/buket.dart
+++ lib/buket.dart
@@ -6,7 +6,7 @@ class buk extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("aaa"),
+      child: Text("aaabbbb"),
     );
   }
 }
