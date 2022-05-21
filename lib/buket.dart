8d597287c58f60547330cc31f881aa18cdfc669b
buketbyk <bukettbayik@gmail.com>
2022-05-21 20:46:11 +0300

    buk1

--- lib/buket.dart
+++ lib/buket.dart
@@ -1,1 +1,12 @@
-sdhufguhdg
+import 'package:flutter/material.dart';
+
+class buk extends StatelessWidget {
+  const buk({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("aaa"),
+    );
+  }
+}
