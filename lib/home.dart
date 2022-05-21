d3be679d02f7a5996bbda92242ede43b8329ffb7
buketbyk <bukettbayik@gmail.com>
2022-05-21 20:57:57 +0300

    feat

---
+++ lib/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class buk extends StatelessWidget {
+  const buk({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("home"),
+    );
+  }
+}
